3c04b868d39e109cf711760753a61198c69d49a3
ndo@$(hostname) <yo@ndo.dev>
2020-07-06 01:01:07 +0200

    test: remove loadable-component

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useTranslation } from 'gatsby-plugin-react-i18next'
 import Partners from './Partners'
-import Loadable from 'react-loadable'
+// import Loadable from 'react-loadable'
+import Testimonial from './Testimonial'
 
-const Testimonial = Loadable({
-  loader: () => import('./Testimonial'),
-  loading() {
-    return <span>Loading...</span>
-  },
-})
+/* const Testimonial = Loadable({ */
+/*   loader: () => import('./Testimonial'), */
+/*   loading() { */
+/*     return <span>Loading...</span> */
+/*   }, */
+/* }) */
 
 const Testimonials = ({ partners, testimonials }) => {
   const { t } = useTranslation()

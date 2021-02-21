959fbc1b4c1377cbb00b54a6c8d52a95d06f4d54
ndom91 <yo@ndo.dev>
2021-02-21 12:42:45 +0100

    fix: clean up + dependency update

--- gatsby-node.js
+++ gatsby-node.js
@@ -79,12 +79,3 @@ exports.createPages = async ({ graphql, actions }) => {
   })
   await createProductPages()
 }
-
-exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
-  // Hack due to Tailwind ^1.1.0 using `reduce-css-calc` which assumes node
-  // https://github.com/bradlc/babel-plugin-tailwind-components/issues/39#issuecomment-526892633
-  const config = getConfig()
-  config.node = {
-    fs: 'empty',
-  }
-}
